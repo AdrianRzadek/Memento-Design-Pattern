b3378361c1e87d161f040baf765758cbeca74668
AdrianRzadek <mschxd@gmail.com>
2023-11-14 14:41:05 +0100

    init

---
+++ memento/src/main/java/source/Memento.java
@@ -0,0 +1,23 @@
+package memento.src.main.java.source;
+
+public class Memento {
+    
+    private String state;
+      
+       
+      
+    public Memento(String state){
+  
+       this.state = state;
+  
+    }
+  
+   
+  
+    public String getState(){
+  
+       return state;
+  
+    }      
+
+}
---
+++ memento/src/main/java/source/Originator.java
@@ -0,0 +1,42 @@
+package memento.src.main.java.source;
+
+
+
+public class Originator {
+
+      private String state;
+
+ 
+
+  public void setState(String state){
+
+     this.state = state;
+
+  }
+
+ 
+
+  public String getState(){
+
+     return state;
+
+  }
+
+ 
+
+  public Memento saveStateToMemento(){
+
+     return new Memento(state);
+
+  }
+
+ 
+
+  public void getStateFromMemento(Memento memento){
+
+     state = memento.getState();
+
+  }
+
+    
+}
